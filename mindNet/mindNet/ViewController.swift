0d86f6659ad88bea34f45564dd04e00d5dded69d
viter-oliver <493501910@qq.com>
2017-04-13 22:50:38 +0800

    testtt
    teett

---
+++ mindNet/mindNet/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  mindNet
+//
+//  Created by viter on 16/9/3.
+//  Copyright © 2016年 viter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var newRefer: UISegmentedControl!
+    var av
+    @IBAction func btnDn(_ sender: AnyObject) {
+        
+    }
+
+    @IBOutlet weak var test: UITextField!
+}
